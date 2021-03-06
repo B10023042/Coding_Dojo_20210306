e0de00b7a3a6e89e8c35a1a63ab333b2a78039a4
firedge2823 <firedge2823@gmail.com>
2021-03-06 11:48:51 +0800

    TimeBox2

---
+++ CodingDojo/SibalaGame.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingDojo
+{
+    public class SibalaGame
+    {
+        public string Play(string gameData)
+        {
+            var player1 = new Dice(gameData.Split("  ").First());
+            var player2 = new Dice(gameData.Split("  ").Last());
+            
+            if (IsDifferentGameResultType(player1, player2))
+            {
+                var winner = player1.GetSamePointCount() > player2.GetSamePointCount() ? player2 : player1;
+                return GetResult(winner);
+            }
+            
+            if (IsTie(player1, player2))
+            {
+                return "Tie.";
+            }
+
+            return GetResult(GetWinner(player1, player2));
+        }
+
+        private static string GetResult(Dice winner)
+        {
+            return $"{winner.Name} wins.{winner.GetDiceType()}:{winner.GetPoint()}";
+        }
+
+        private static bool IsDifferentGameResultType(Dice player1, Dice player2)
+        {
+            return player1.GetSamePointCount() != player2.GetSamePointCount();
+        }
+
+        private static bool IsTie(Dice player1, Dice player2)
+        {
+            return player1.GetPoint() == player2.GetPoint();
+        }
+
+        private static Dice GetWinner(Dice player1, Dice player2)
+        {
+            var winner = player1.GetPoint() > player2.GetPoint() ? player1 : player2;
+            return winner;
+        }
+    }
+
+    public class Dice
+    {
+        public string Name { get; set; }
+        private List<int> Points { get; set; }
+
+        public Dice(string gameData)
+        {
+            this.Name = gameData.Split(":").First();
+            this.Points = gameData.Split(":").Last().Split().Select(int.Parse).ToList();
+        }
+
+        public int GetPoint()
+        {
+            if (GetSamePointCount() == 3)
+            {
+                return Points.GroupBy(p => p)
+                    .Where(x => x.Count() == 1)
+                    .Sum(x => x.Key);
+            }
+            
+            if (GetSamePointCount() == 2)
+            {
+                return Points.GroupBy(p => p)
+                    .Max(p => p.Key) * 2;
+            }
+
+            return Points.First();
+        }
+
+        public int GetSamePointCount()
+        {
+            return Points.GroupBy(p => p).Count();
+        }
+
+        public string GetDiceType()
+        {
+            return GetSamePointCount() == 1 ? "all the same kind" : "normal point";
+        }
+    }
+}
--- Coding_Dojo_Tests/UnitTest1.cs
+++ Coding_Dojo_Tests/UnitTest1.cs
@@ -1,18 +1,66 @@
+using CodingDojo;
 using NUnit.Framework;
 
 namespace Coding_Dojo_Tests
 {
-    public class Tests
+    public class SibalaTests
     {
+        private SibalaGame _game;
+
         [SetUp]
-        public void Setup()
+        public void SetUp()
         {
+            _game = new SibalaGame();
         }
 
         [Test]
-        public void Test1()
+        public void all_the_same_kind_Amy_win()
         {
-            Assert.Pass();
+            var gameResult = _game.Play("Amy:6 6 6 6  Lin:2 2 2 2");
+            Assert.AreEqual("Amy wins.all the same kind:6", gameResult);
+        }
+
+        
+        [Test]
+        public void all_the_same_kind_Lin_win()
+        {
+            var gameResult = _game.Play("Amy:1 1 1 1  Lin:2 2 2 2");
+            Assert.AreEqual("Lin wins.all the same kind:2", gameResult);
+        }
+        
+        [Test]
+        public void all_the_same_kind_tie()
+        {
+            var gameResult = _game.Play("Amy:2 2 2 2  Lin:2 2 2 2");
+            Assert.AreEqual("Tie.", gameResult);
+        }
+
+        [Test]
+        public void all_the_same_kind_and_no_points()
+        {
+            var gameResult = _game.Play("Amy:5 2 3 4  Lin:2 2 2 2");
+            Assert.AreEqual("Lin wins.all the same kind:2", gameResult);
+        }
+        
+        [Test]
+        public void normal_points_and_no_points_4()
+        {
+            var gameResult = _game.Play("Amy:4 4 1 3  Lin:1 2 3 4");
+            Assert.AreEqual("Amy wins.normal point:4", gameResult);
+        }
+        
+        [Test]
+        public void normal_points_and_no_points_2()
+        {
+            var gameResult = _game.Play("Amy:2 2 1 3  Lin:1 2 3 4");
+            Assert.AreEqual("Amy wins.normal point:4", gameResult);
+        }
+        
+        [Test]
+        public void normal_points_and_no_points_6()
+        {
+            var gameResult = _game.Play("Amy:3 3 6 6  Lin:1 2 3 4");
+            Assert.AreEqual("Amy wins.normal point:12", gameResult);
         }
     }
 }
