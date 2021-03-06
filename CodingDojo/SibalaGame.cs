559ae83760d6a11657c4af4a66b9d45e915289cb
Cloud Chen <cloud.chen@titansoft.com.sg>
2021-03-06 10:43:23 +0800

    implement

---
+++ CodingDojo/SibalaGame.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CodingDojo
+{
+    public class SibalaGame
+    {
+        public string Play(string gameData)
+        {
+            var dice1 = new Dice(gameData.Split("  ").First());
+            var dice2 = new Dice(gameData.Split("  ").Last());
+
+            if (dice1.DiceType != dice2.DiceType)
+            {
+
+            }
+            if (dice2.Points.First() == dice1.Points.First())
+            {
+                return "Tie.";
+            }
+
+
+            return $"{GetWinner(dice2, dice1).PlayerName} wins. all the same kind:{GetWinner(dice2, dice1).Points.First()}";
+        }
+
+        private static Dice GetWinner(Dice dice2, Dice dice1)
+        {
+            var winner = dice2.Points.First() > dice1.Points.First()
+                ? dice2
+                : dice1;
+
+            return winner;
+        }
+    }
+}
+
+public class Dice
+{
+    public Dice(string gameData)
+    {
+        this.Points = gameData.Split(":").Last().Split().Select(int.Parse).ToList();
+        this.PlayerName = gameData.Split(":").First();
+        this.DiceType = this.Points.GroupBy(x => x).Count() == 1 ? DiceType.AllTheSameKind : DiceType.NoPoints;
+    }
+
+    public DiceType DiceType { get; set; }
+
+    public string PlayerName { get; set; }
+
+    public List<int> Points { get; set; }
+}
+
+public enum DiceType
+{
+    NoPoints,
+    AllTheSameKind
+}
--- Coding_Dojo_Tests/UnitTest1.cs
+++ Coding_Dojo_Tests/UnitTest1.cs
@@ -1,18 +1,41 @@
+using CodingDojo;
 using NUnit.Framework;
 
 namespace Coding_Dojo_Tests
 {
-    public class Tests
+    public class SibalaGameTests
     {
-        [SetUp]
-        public void Setup()
+        [Test]
+        public void when_all_the_same_kind_then_winner_is_Lin()
         {
+            var sibalaGame = new SibalaGame();
+            var winnerResult = sibalaGame.Play("Amy:1 1 1 1  Lin:4 4 4 4");
+            Assert.AreEqual("Lin wins. all the same kind:4", winnerResult);
         }
 
         [Test]
-        public void Test1()
+        public void when_all_the_same_kind_then_winner_is_amy()
         {
-            Assert.Pass();
+            var sibalaGame = new SibalaGame();
+            var winnerResult = sibalaGame.Play("Amy:3 3 3 3  Lin:2 2 2 2");
+            Assert.AreEqual("Amy wins. all the same kind:3", winnerResult);
+        }
+
+        [Test]
+        public void when_all_the_same_kind_and_tie()
+        {
+            var sibalaGame = new SibalaGame();
+            var winnerResult = sibalaGame.Play("Amy:1 1 1 1  Lin:1 1 1 1");
+            Assert.AreEqual("Tie.", winnerResult);
+        }
+
+
+        [Test]
+        public void when_all_the_same_kind_and_no_point()
+        {
+            var sibalaGame = new SibalaGame();
+            var winnerResult = sibalaGame.Play("Amy:1 1 1 1  Lin:4 3 2 1");
+            Assert.AreEqual("Amy wins. all the same kind:1", winnerResult);
         }
     }
 }
